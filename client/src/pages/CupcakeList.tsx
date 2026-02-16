a686f6f0e949cc75d06e64636ca5576f1a633723
Kevan Gautier <kevangautier@gmail.com>
2026-02-16 10:50:14 +0100

    etape 3 réalisée

--- client/src/pages/CupcakeList.tsx
+++ client/src/pages/CupcakeList.tsx
@@ -27,6 +27,18 @@ function CupcakeList() {
       });
   }, []);
 
+  type AccessoryArray = { id: number; name: string; slug: string }[];
+  const [accessories, setAccessories] = useState<AccessoryArray>([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3310/api/accessories")
+      .then((response) => response.json())
+      .then((data) => {
+        console.info("accesoires récupérés:", data);
+        setAccessories(data as AccessoryArray);
+      });
+  }, []);
+
   // Step 3: get all accessories
 
   // Step 5: create filter state
