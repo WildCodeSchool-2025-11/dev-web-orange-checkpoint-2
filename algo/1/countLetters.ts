e8b81bc88ace610972f51b40d5165edf32131932
Victor dS <victordespirlet@gmail.com>
2026-02-16 12:17:25 +0100

    Algo 1 done

--- algo/1/countLetters.ts
+++ algo/1/countLetters.ts
@@ -11,7 +11,15 @@ Exemples :
 
 function countLetters(givenString: string, letter: string): number {
   // Ton code ici !
-  return 0;
+  let letterCount = 0;
+
+  for (let i = 0; i < givenString.length; i++) {
+    if (givenString[i] === letter) {
+      letterCount++;
+    }
+  }
+
+  return letterCount;
 }
 
 export default countLetters;
