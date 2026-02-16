5b901e75571fba00904b4d31d92c53db74ef1830
sophiemorales6440 <sophiemorales6440@github.com>
2026-02-16 10:19:09 +0100

    finished algo2_test_ok

--- algo/2/getFibonacciSequence.ts
+++ algo/2/getFibonacciSequence.ts
@@ -10,9 +10,25 @@ Détails
 Si n est inférieur ou égal à 0, la fonction doit retourner un tableau vide []
 */
 
-function getFibonacciSequence(size: number): number[] {
-  // Ton code ici !
-  return [0, 1];
+function getFibonacciSequence(n: number): number[] {
+  if (n <= 0) {
+    return [];
+  }
+  if (n === 1) {
+    return [0];
+  }
+  if (n === 2) {
+    return [0, 1];
+  }
+
+  const tableau = [0, 1];
+  for (let i = 2; i < n; i++) {
+    const avantDernier = tableau[tableau.length - 2];
+    const dernier = tableau[tableau.length - 1];
+    const nouveauNombre = avantDernier + dernier;
+    tableau.push(nouveauNombre);
+  }
+  return tableau;
 }
 
 export default getFibonacciSequence;
